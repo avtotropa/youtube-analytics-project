1aa96f4c502868440f131261f5389fafd48eea15
autotropa <autotropa@yandex.ru>
2023-04-10 20:09:06 +0300

    fix channel.py & added channel_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,19 @@
-
+import os
+import requests
+import json
 
 class Channel:
     """Класс для ютуб-канала"""
+    API_KEY = os.environ.get('YT_API_KEY')
+
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        response = requests.get(
+            f'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={self.API_KEY}')
+        with open('channel_info.json', 'w', encoding='utf-8') as f:
+            json.dump(response.json(), f, ensure_ascii=False, indent=4)
