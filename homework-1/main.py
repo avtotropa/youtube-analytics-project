77608b31aed66734df350b4b5bbc90636f05738d
autotropa <autotropa@yandex.ru>
2023-04-11 16:08:49 +0300

    redone  class Channel

--- homework-1/main.py
+++ homework-1/main.py
@@ -3,7 +3,7 @@ from src.channel import Channel
 if __name__ == '__main__':
 
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
-    vdud.print_info()
+    vdud.print_info('UCMCgOm8GZkHp8zJ6l7_hIuA')
 
     """
 {
--- src/channel.py
+++ src/channel.py
@@ -1,19 +1,20 @@
 import os
-import requests
-import json
+
+from googleapiclient.discovery import build
+from helper.youtube_api_manual import youtube
+
 
 class Channel:
     """Класс для ютуб-канала"""
     API_KEY = os.environ.get('YT_API_KEY')
-
+    youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
 
-    def print_info(self) -> None:
+    def print_info(self, channel_id) -> None:
         """Выводит в консоль информацию о канале."""
-        response = requests.get(
-            f'https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id={self.channel_id}&key={self.API_KEY}')
-        with open('channel_info.json', 'w', encoding='utf-8') as f:
-            json.dump(response.json(), f, ensure_ascii=False, indent=4)
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        print(channel)
